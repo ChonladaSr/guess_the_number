1e5055ad4143d0d0f3223f69878fbf7b5dc6a29f
USER <srisook_c@silpakorn.edu>
2022-12-23 15:58:01 +0700

    save

--- lib/game.dart
+++ lib/game.dart
@@ -1,24 +1,22 @@
 // ignore_for_file: avoid_print
-
 import 'dart:math';
 
 // enumeration
-enum GuessResult {
-  correct, tooHigh, tooLow
-}
+enum GuessResult { correct, tooHigh, tooLow }
 
 class Game {
-  final int answer = Random().nextInt(100) + 1;
+  //ชื่อคลาสไม่จำเป็นต้องเป็นชื่อเดียวกันกับไฟล์
+  final int answer;
+  //final String play;
+
   int _totalGuesses = 0;
 
-  // constructor
-  Game() {
+  Game({int maxRandom = 100}) : answer = Random().nextInt(maxRandom) + 1 {
     print('Answer is $answer');
   }
 
   GuessResult doGuess(int guess) {
     _totalGuesses++;
-
     if (guess == answer) {
       return GuessResult.correct;
     } else if (guess > answer) {
@@ -28,7 +26,11 @@ class Game {
     }
   }
 
-  // getter
+  // getter method
+  int getTotalGuesses() {
+    return _totalGuesses;
+  }
+
   int get totalGuesses {
     return _totalGuesses;
   }
--- lib/guess_the_number.dart
+++ lib/guess_the_number.dart
@@ -1,8 +1,7 @@
 // ignore_for_file: avoid_print
-
 import 'dart:io';
 
-import 'game.dart';
+import 'package:hello_flutter1/game.dart';
 
 void main() {
   var game = Game();
@@ -11,7 +10,6 @@ void main() {
   print('╔══════════════════════════════════════════════════');
   print('║              GUESS THE NUMBER                    ');
   print('║──────────────────────────────────────────────────');
-
   do {
     stdout.write('║  Guess the number between 1 and 100 : ');
 
@@ -30,12 +28,12 @@ void main() {
     }
 
     guessResult = game.doGuess(guess);
-
     if (guessResult == GuessResult.correct) {
       print('║  ➜ $guess is CORRECT ❤, total guesses: ${game.totalGuesses}');
       print('║──────────────────────────────────────────────────');
       print('║                    THE END                       ');
       print('╚══════════════════════════════════════════════════');
+      print('Play again? (Y/N): ');
     } else if (guessResult == GuessResult.tooHigh) {
       print('║  ➜ $guess is TOO HIGH! ▲');
       print('║──────────────────────────────────────────────────');
